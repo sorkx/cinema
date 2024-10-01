036efe0a3e08b054a965119cd5ceda003155d872
SORQ1NGG <daniil.sork@gmail.com>
2024-10-01 18:03:02 +0300

    Added try catch for staff

--- src/entities/Staff/model/staff.js
+++ src/entities/Staff/model/staff.js
@@ -10,29 +10,40 @@ export const useStaffStore = defineStore('staff', () => {
     const person = ref([])
 
     const fetchStaff = async (filmId) => {
-        const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=${filmId}`, {
-            method: 'GET',
-            headers: {
-                'X-API-KEY': import.meta.env.VITE_API_KEY,
-                'Content-Type': 'application/json',
-            },
-        })
-        const data = await response.json()
-        staff.value = data
+        try {
+            const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=${filmId}`, {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': import.meta.env.VITE_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+            const data = await response.json()
+	
+            staff.value = data
+	
+            console.log('staff', staff.value)
+        } catch(e) {
+            console.log('Ошибка получения персонала', e)
+        }
     }
 
     const fetchCurrentPerson = async (id) => {
-        const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v1/staff/${id}`, {
-            method: 'GET',
-            headers: {
-                'X-API-KEY': import.meta.env.VITE_API_KEY,
-                'Content-Type': 'application/json',
-            },
-        })
-        const data = await response.json()
-        person.value = data
-
-        console.log('person.value', person.value)
+        try {
+            const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v1/staff/${id}`, {
+                method: 'GET',
+                headers: {
+                    'X-API-KEY': import.meta.env.VITE_API_KEY,
+                    'Content-Type': 'application/json',
+                },
+            })
+            const data = await response.json()
+            person.value = data
+	
+            console.log('person.value', person.value)
+        }catch(e) {
+            console.log('Ошибка получения персоны', e)
+        }
     }
 
     return {
