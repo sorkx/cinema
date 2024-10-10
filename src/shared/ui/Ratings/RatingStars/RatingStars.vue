62f014c7a726c3e595f023f363c6f1a23a0649ad
Даниил Ворокосов <daniilvorokosov@mail.ru>
2024-10-10 03:26:02 +0300

    Update rating stars

--- src/shared/ui/Ratings/RatingStars/RatingStars.vue
+++ src/shared/ui/Ratings/RatingStars/RatingStars.vue
@@ -1,5 +1,8 @@
 <script setup>
 import {
+    computed,
+} from 'vue'
+import {
     starsSystem,
 } from './model'
 import {
@@ -9,23 +12,35 @@ import {
 const props = defineProps({
     rating: {
         type: Number,
-        required: true
+        required: true,
+        validator: (value) => value >= 0 && value <= starsSystem
     },
+})
+
+const ratingWidth = computed(() => {
+    return props.rating / starsSystem * 100
+})
+
+const ratingWidthStyle = computed(() => {
+    return `width: ${ratingWidth.value}%`
 })
 </script>
 
 <template>
-	<div class="stars">
-		<div 
-			v-for="item in starsSystem"
-			:key="item"
-			class="stars__item"
-		>
-			<UISymbol
+	<div class="star-rating">
+		<span v-for="index in starsSystem" :key="index">
+			<UISymbol 
 				name="star"
-				:class="item <= props.rating ? 'active' : 'inactive'" 
-				:fill="item <= props.rating ? 'currentColor' : 'none'"  
+				class="star star--outlined" 
 			/>
+		</span>
+		<div class="star-rating__colored" :style="ratingWidthStyle">
+			<span v-for="index in starsSystem" :key="index">
+				<UISymbol 
+					name="star"
+					class="star star--colored" 
+				/>
+			</span>
 		</div>
 	</div>
 </template>
